0bcd8043df05f024be3b09717db51aec1b79c323
Julia-BARGOIN <julia.bargoin@tuta.io>
2020-09-23 11:25:36 +0200

    feat(varible): exo variable js

---
+++ variable/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// My Digital School Forever
+
+/*
+* My Digital School 
+* forever every day
+*/
+
+var string = '42';
+var number = 42;
+var float  = 42.0;
+var boolean  = true;
+var array = ['42', '42', '24'];
+var date = '42';
+var objet = {
+  number,
+  date,
+  array
+};
+
+var my42count = 'quarante-deux';
+
+var myArray42 = ['q', 'u', 'a', 'r', 'a', 'n', 't', 'e', '-', 'd', 'e', 'u', 'x'];
+
+var a = 24;
+var b = 42;
+var A = b;
+var B = a;
+
+//console.log(string, number, float, boolean, array, date, objet);
+//console.log(my42count.length);
+//console.log(typeof string);
+console.log("var a "+ A, "var b " + B);
