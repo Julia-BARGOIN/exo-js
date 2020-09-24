ffd8ab5d4f4db150fc819fcc2b101a91745ec7c7
Julia-BARGOIN <julia.bargoin@tuta.io>
2020-09-24 13:07:22 +0200

    feat(boucle): exo js boucle

--- variable/index.js
+++ variable/index.js
@@ -1,34 +1,76 @@
 'use strict';
 
+// Erceice 1
 // My Digital School Forever
 
-/*
-* My Digital School 
-* forever every day
-*/
+// Exercice 2
+/* 
+ * My Digital School forever every day
+ */
 
+// Exercice 3
 var string = '42';
 var number = 42;
-var float  = 42.0;
-var boolean  = true;
-var array = ['42', '42', '24'];
-var date = '42';
-var objet = {
-  number,
-  date,
-  array
-};
+var float = 42.00;
+var boolean = true;
+var array = ['42'];
+var date = new Date(4241, 12, 1, 24, 42, 42);
+var object = {'42': 42};
+var und = undefined;
+var empty = null;
 
+console.log(string, number, float, boolean, array, date, object, und, empty);
+
+// Exercice 4
 var my42count = 'quarante-deux';
 
-var myArray42 = ['q', 'u', 'a', 'r', 'a', 'n', 't', 'e', '-', 'd', 'e', 'u', 'x'];
+console.log(my42count.length);
 
+// Exercice 5
+var value;
+var check = value || 42;
+
+console.log(check);
+
+// Exercice 6
+var myArray42 = ['q', 'u', 'a', 'r', 'a', 'n', 't', 'e', '-', 'd', 'e', 'u', 'x']
+
+console.log(myArray42[0]);
+
+// Exercice 7
+var myArray42Len = myArray42.length;
+
+console.log(myArray42Len);
+
+// Exercice 8
+console.log(myArray42.join('') + ' La grande réponse sur la vie, l’univers et le reste !');
+
+// Exercice 9
+var rand = Math.floor(Math.random() * (42)) + 1
+
+console.log(rand === 42);
+
+// Exercice 10
+console.log('Exercice 10 --> ', typeof 'sdfsdfsdfsd', typeof number, typeof float, typeof boolean, array, date, object);
+
+// Exercice 11
+var compute42 = 7 * 6;
+console.log('Exercice 11 --> ', compute42.toString());
+
+// Exercice 12
+var number42 = '42424242 sdfsdf sd 42';
+var test = number42.replace(/42/gi, 'quarante-deux ');
+console.log(test);
+
+// Exercice 13
 var a = 24;
 var b = 42;
-var A = b;
-var B = a;
+var c = a;
 
-//console.log(string, number, float, boolean, array, date, objet);
-//console.log(my42count.length);
-//console.log(typeof string);
-console.log("var a "+ A, "var b " + B);
+a = b;
+b = c;
+
+console.log(a, b);
+
+
+
